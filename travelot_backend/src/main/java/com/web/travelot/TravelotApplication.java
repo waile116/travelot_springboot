455ada07bce572a0a5e1bc4e925a6b4cd6a62576
TOO LE <toowlwl@gmail.com>
2025-01-23 20:31:36 +0800

    first commit

---
+++ travelot_backend/src/main/java/com/web/travelot/TravelotApplication.java
@@ -0,0 +1,13 @@
+package com.web.travelot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelotApplication.class, args);
+	}
+
+}
---
+++ travelot_backend/src/test/java/com/web/travelot/TravelotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.travelot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
