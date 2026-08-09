3abda49bf20fe396639aafe7b13c6562ad4844e2
unknown <willytwl1106@gmail.com>
2026-08-09 23:39:19 +0800

    Update main.js

--- travelot_frontend/src/main.js
+++ travelot_frontend/src/main.js
@@ -17,7 +17,8 @@ import {
 } from "./common.js";
 
 // axios base URL for backend server
-axios.defaults.baseURL = "travelotspringboot-production.up.railway.app:8080"; //"http://localhost:8080/travelot/";
+axios.defaults.baseURL =
+  "https://travelotspringboot-production.up.railway.app:8080"; //"http://localhost:8080/travelot/";
 
 const app = createApp(App);
 
